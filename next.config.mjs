3ddf370246e6a799e83f9f43dab8cdef033539a4
shivkumarsain <shivkumarsain2007@gmail.com>
2026-01-22 22:31:12 +0530

    fix: update nextjs security patch

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,5 @@
-<<<<<<< HEAD
 const nextConfig = {
   output: "export",
 }
 
 export default nextConfig
-=======
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
->>>>>>> 1078a7a948a7aed351b7af1e07a87179f784b0cf
